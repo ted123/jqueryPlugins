d55883242f7b2207a47e71596591ec2156eef24d
Dedito Cantila Jr <dedito.cantila@gmail.com>
2014-09-15 16:14:34 +0800

    Added few modifications on the implementation.

--- addBackdrop.js
+++ addBackdrop.js
@@ -1,21 +1,30 @@
 (function ( $ ) {
 
-	$.fn.addBackdrop = function ( zindex1, zindex2, bdFunc ) {
-		var backdrop  = '<div id="backdrop-compact"></div>';
-		var clickFunc = bdFunc || function () {
-			this.remove();
-		} 
+	//var $ = require( 'jquery' );
 
-		if ( $( '#backdrop-compact' ).length ) {
+	$.fn.addBackdrop = function ( options ) {
+		var exempted  = options.exempted;
+		var backdrop  = '<div id="backdrop-compact"></div>';
+		var clickFunc = options.bdFunc || function () {
+			this.remove();
+		}
+
+		for ( var i = 0; i < exempted.length; i++ ) {
+			$( exempted[i] ).css( 'z-index', options.zindex1 );
+		}
+
+		if ( $( '#backdrop-compact' ).is( ':visible' ) ) {
 			$( '#backdrop-compact' ).off();
 			$( '#backdrop-compact' ).remove();
 		}
 
 		$( 'body' ).append( backdrop );
 
+		$( 'backdrop-compact' ).css( 'z-index', options.zindex2  );
+
 		$( 'backdrop-compact' ).on( 'click', clickFunc );
 
-		return this.css( 'z-index', zindex1 );
+		return this.css( 'z-index', options.zindex1 );
 	};
- 
+
 }( jQuery ));
--- compactButton.js
+++ compactButton.js
@@ -1,29 +1,36 @@
 (function ( $ ) {
+	'use strict';
 
-	$.fn.compactButton = function ( height, bdClick ) {
+	var $     = require( 'jquery' );
+	var _     = require( 'underscore' );
+	var utils = require( 'videoPlayer/utils/utils' );
+
+	$.fn.compactToButton = function ( options ) {
 		var self       = this;
-		
+
 		var wheight    = $( window ).height();
 		var wwidth     = $( window ).width();
 
 		var btnClass   = 'btn btn-compact';
 		var listClass  = 'list-compact';
-		var icon       = '&emsp;<span class="caret"></span>';
-		var backdrop   = '<div id="backdrop-compact" ></div>'  
+		var icon       = '<span class="caret"></span>';
+		var backdrop   = '<div id="backdrop-compact"></div>'
 
 		var firstChild = this.children().first();
-		var lastChild  = this.children().last();
+		var lastChild  = this.children().not( firstChild );
 
-		var forigin    = firstChild[0];
+		var mobileCon  = wheight !== $( window ).height() && wwidth !== $( window ).width();
 
-		var funcClick  = bdClick || function () {
+		// option params
+		var height       = options.height;
+		var funcOnResize = options.onresize || function () {};
+		var funcClick    = options.onBdClick || function () {
 			this.remove();
 			lastChild.hide();
 		}
 
 		function conditionals () {
-			console.log( self.height() +' - '+height+' - '+firstChild.hasClass( btnClass )  );
-			if ( self.height() > height && !firstChild.hasClass( btnClass )  ) {
+			if ( lastChild.height() > height && !firstChild.hasClass( btnClass )  ) {
 				firstChild.addClass( btnClass ).append( icon );
 				lastChild.addClass( listClass );
 				$( 'body' ).append( backdrop );
@@ -35,10 +42,10 @@
 				} );
 				$( '#backdrop-compact' ).on( 'click', funcClick );
 
-			} else if ( self.height() <= height && firstChild.hasClass( btnClass ) ){
-				firstChild.html( forigin );
+			} else if ( lastChild.height() <= height && firstChild.hasClass( btnClass ) ){
+				firstChild.removeClass( btnClass ).children().last().remove();
 				lastChild.removeClass( listClass );
-				$( '#backdrop-compact' ).remove(); 
+				$( '#backdrop-compact' ).remove();
 
 				lastChild.show();
 
@@ -46,18 +53,22 @@
 			}
 		}
 
-		conditionals();
-
-		$( window ).on( 'resize', function () {
-			if ( wheight !== $( window ).height() && wwidth !== $( window ).width() ) {
-				if( !self.length ) {
+		function doOnResize () {
+			if ( mobileCon || !utils.isMobile() ) {
+				if( !self.is( ':visible' ) ) {
 					$( window ).off( 'resize' );
 					return;
-				} 
+				}
+				funcOnResize();
 				conditionals();
-			}           
+			}
+		}
 
-		} );
+		conditionals();
+
+		$( window ).on( 'resize', _.debounce( doOnResize, 500 ) );
+
+		return this;
 	};
- 
+
 }( jQuery ));
